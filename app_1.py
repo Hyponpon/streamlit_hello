912d5c9cca1227ff73fcc46f893433b7b8abaa6b
Hyponpon <145899359+Hyponpon@users.noreply.github.com>
2024-09-28 18:19:16 +0900

    Add files via upload

--- app_1.py
+++ app_1.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import japanize_matplotlib
+# import japanize_matplotlib
 import pulp
 from ShiftScheduler import ShiftScheduler
 ss = ShiftScheduler()
